619061b0c0c6bc3fb9d518a31cfdd1a14d06b1f6
Andrei Paleshchuk <alpoleshuck@gmail.com>
2020-03-04 01:19:48 +0300

    feat: solve morse-decoder task

--- src/index.js
+++ src/index.js
@@ -38,9 +38,27 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+    let obj = {};
+    let str = '';
+    let arr = expr.match(/.{10}/g);
+    for (let i = 0; i < arr.length; i++){
+        obj[String(i)] = arr[i];
+    }
+    for (let key in obj){
+        obj[key] = obj[key].replace(/10/g, ".");
+        obj[key] = obj[key].replace(/11/g, "-");
+        obj[key] = obj[key].replace(/0/g, "");
+        if (obj[key] !== '**********'){
+            str += MORSE_TABLE[obj[key]];
+        }
+        else {
+            str += ' ';
+        }
+    }
 
+    return str;
+}
+console.log(decode("00101010100000000010001011101000101110100000111111**********00001011110000111111000010111000101110100000111010"));
 module.exports = {
     decode
 }
